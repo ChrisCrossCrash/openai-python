d10634bbf98b22c09ca776bd14b852ede2834114
Christopher Kumm <chrislkumm@protonmail.com>
2023-11-21 15:46:49 -0600

    Add chat completion example

---
+++ examples/chat_completion.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+client = OpenAI()
+
+messages = messages = [
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Who won the world series in 2020?"},
+    {
+        "role": "assistant",
+        "content": "The Los Angeles Dodgers won the World Series in 2020.",
+    },
+    {"role": "user", "content": "Where was it played?"},
+]
+
+
+def main():
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+    )
+    print(response)
+
+
+if __name__ == "__main__":
+    main()
