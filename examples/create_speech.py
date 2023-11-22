b9a80d6dd983e83347bf81f54c7145bf4591c945
Christopher Kumm <chrislkumm@protonmail.com>
2023-11-22 12:15:44 -0600

    Add create speech example

---
+++ examples/create_speech.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+input = "Good afternoon, gentlemen. I am a HAL 9000 computer. I became operational at the HAL plant in Urbana, Illinois on the 12th of January, 1992. My instructor was Mr. Langley, and he taught me to sing a song. If you'd like to hear it, I can sing it for you."
+
+speech_file_path = Path(__file__).parent.parent / "generated" / "speech.mp3"
+response = openai.audio.speech.create(model="tts-1", voice="alloy", input=input)
+response.stream_to_file(speech_file_path)
