ef5dd70a22ac72b8330ef1012b7bf241daa1761e
Christopher Kumm <chrislkumm@protonmail.com>
2023-11-22 12:16:22 -0600

    Improve file path

--- examples/transcription.py
+++ examples/transcription.py
@@ -1,12 +1,13 @@
 from dotenv import load_dotenv
 from openai import OpenAI
+from pathlib import Path
 
 load_dotenv()
 
 client = OpenAI()
 
 if __name__ == "__main__":
-    with open("../data/recording.mp3", "rb") as f:
+    with open(f"{Path(__file__).parent}/../data/recording.mp3", "rb") as f:
         transcript = client.audio.transcriptions.create(model="whisper-1", file=f)
 
     print(transcript.text)
