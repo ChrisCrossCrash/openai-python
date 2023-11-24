7d951fda01a9ebdd2ea73af669457364f42dc73e
Christopher Kumm <chrislkumm@protonmail.com>
2023-11-24 14:48:43 -0600

    Change AI voice

--- examples/create_speech.py
+++ examples/create_speech.py
@@ -7,5 +7,5 @@ load_dotenv()
 input = "Good afternoon, gentlemen. I am a HAL 9000 computer. I became operational at the HAL plant in Urbana, Illinois on the 12th of January, 1992. My instructor was Mr. Langley, and he taught me to sing a song. If you'd like to hear it, I can sing it for you."
 
 speech_file_path = Path(__file__).parent.parent / "generated" / "speech.mp3"
-response = openai.audio.speech.create(model="tts-1", voice="alloy", input=input)
+response = openai.audio.speech.create(model="tts-1", voice="echo", input=input)
 response.stream_to_file(speech_file_path)
